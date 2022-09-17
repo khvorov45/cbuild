2c89068d9e4c179cd090b12ec0886543af5cc6f4
Arseniy Khvorov <khvorov45@gmail.com>
2022-09-17 18:35:46 +1000

    add coverage reporting

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,6 @@
-gcc -g -Wall -Wextra \
+rm -f tests/*gcno
+rm -f tests/*gcda
+gcc -g -Wall -Wextra -O0 \
     -Wno-missing-field-initializers -Wno-unused-parameter \
-    tests/tests.c -o tests/tests.bin && tests/tests.bin
+    --coverage -fno-inline -fno-inline-small-functions -fno-default-inline -dumpbase '' \
+    tests/tests.c -o tests/tests.bin && tests/tests.bin && gcov tests/tests.c
