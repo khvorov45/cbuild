8ec8e6dd5e29d5fcdd8514a1d2dd6cb23f6441aa
Arseniy Khvorov <khvorov45@gmail.com>
2022-12-31 16:51:47 +1100

    switch linux scripts to clang for consistency

--- example/build.sh
+++ example/build.sh
@@ -1,3 +1,3 @@
 SCRIPT_DIR=$(dirname "$0")
 RUN_BIN=$SCRIPT_DIR/build.bin
-gcc -g -Wall -Wextra -Werror -Wfatal-errors $SCRIPT_DIR/build.c -o $RUN_BIN -lpthread && $RUN_BIN $@
+clang -g -Wall -Wextra -Werror -Wfatal-errors $SCRIPT_DIR/build.c -o $RUN_BIN -lpthread && $RUN_BIN $@
